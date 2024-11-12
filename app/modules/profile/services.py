2670adf3f599217759191791882eb299cd4e9a2a
Rocio Lopez Moyano <rociolopez08@gmail.com>
2024-11-12 21:59:46 +0100

    WI-viewUserProfile repositories and services

--- app/modules/profile/services.py
+++ app/modules/profile/services.py
@@ -1,7 +1,6 @@
 from app.modules.profile.repositories import UserProfileRepository
 from core.services.BaseService import BaseService
 
-
 class UserProfileService(BaseService):
     def __init__(self):
         super().__init__(UserProfileRepository())
@@ -10,5 +9,10 @@ class UserProfileService(BaseService):
         if form.validate():
             updated_instance = self.update(user_profile_id, **form.data)
             return updated_instance, None
-
         return None, form.errors
+
+    def get(self, user_id):
+        return self.repository.get_by_id(user_id)  
+
+    def search_users(self, query):
+        return self.repository.search_by_name_or_surname(query)
