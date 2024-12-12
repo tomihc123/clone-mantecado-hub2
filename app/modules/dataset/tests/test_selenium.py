4adf1f96a22899eea9a2d0b1c896ad0583d1ca71
josmorgue3 <josmorgue3@alum.us.es>
2024-12-12 19:09:51 +0100

    Test: Nuevo test selenium que prueba rate datasets

--- app/modules/dataset/tests/test_selenium.py
+++ app/modules/dataset/tests/test_selenium.py
@@ -2,6 +2,7 @@ import os
 import time
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -131,5 +132,54 @@ def test_upload_dataset():
         close_driver(driver)
 
 
+def test_rate_dataset():
+    driver = initialize_driver()
+
+    try:
+        host = get_host_for_selenium_testing()
+
+        # Paso 1: Loguearse
+        driver.get(f"{host}/login")
+        wait_for_page_to_load(driver)
+
+        email_field = driver.find_element(By.NAME, "email")
+        password_field = driver.find_element(By.NAME, "password")
+
+        email_field.send_keys("user1@example.com")
+        password_field.send_keys("1234")
+        password_field.send_keys(Keys.RETURN)
+        wait_for_page_to_load(driver)
+
+        # Paso 2: Navegar al primer dataset
+        dataset_url = f"{host}/doi/10.1234/dataset4/"
+        driver.get(dataset_url)
+        wait_for_page_to_load(driver)
+
+        # Paso 3: Pulsar en el botón con id "rate-button"
+        primary_button = driver.find_element(By.ID, "rate-button")
+        primary_button.click()
+        wait_for_page_to_load(driver)
+
+        # Paso 4: Pulsar en el input con id 3 (en el popup)
+        popup_input = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "3"))
+        )
+        popup_input.click()
+        wait_for_page_to_load(driver)
+
+        # Paso 5: Pulsar en el botón con class "btn btn-primary" dentro del modal: "modal-footer"
+        modal_footer = driver.find_element(By.CLASS_NAME, "modal-footer")
+        submit_button = modal_footer.find_element(By.CLASS_NAME, "btn-primary")
+        submit_button.click()
+
+        wait_for_page_to_load(driver)
+
+        print("Test passed!")
+
+    finally:
+        close_driver(driver)
+        
+
 # Call the test function
 test_upload_dataset()
+test_rate_dataset()
